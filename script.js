70c5b4cd999cd59d6564732e8f62f1e47fcf7038
Rakshit Raj~ <rakshit.raj.cg@gmail.com>
2025-09-23 12:15:49 +0530

    js change

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelectorAll("nav a");
   const currentPage = window.location.pathname.split("/").pop(); 
-  // e.g. "action.html"
 
   links.forEach(link => {
-    // Remove old active class
     link.classList.remove("active");
 
-    // If link matches current page, add active
     if (link.getAttribute("href") === currentPage) {
       link.classList.add("active");
     }
