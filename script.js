f00d8c669bb7d9800dec1626ab1724bcbc4aa732
Rakshit Raj~ <rakshit.raj.cg@gmail.com>
2025-09-24 09:59:50 +0530

    new img and movie added in superhero

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelectorAll("nav a");
   const currentPage = window.location.pathname.split("/").pop(); 
+  
 
   links.forEach(link => {
+
     link.classList.remove("active");
+
 
     if (link.getAttribute("href") === currentPage) {
       link.classList.add("active");
